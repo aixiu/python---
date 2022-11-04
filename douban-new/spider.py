40aac00439ee9c255a4613c72fc283e1251b06d5
aixiu <4815563@qq.com>
2022-11-04 22:37:46 +0800

    updata

---
+++ douban-new/spider.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Aixiu
+# @Time  : 2022/11/03 11:38:12
+
+# 视频地址：https://www.bilibili.com/video/BV12E411A7ZQ/?p=16&vd_source=20f7c1f5f32f90ae92d9428e45039d9b
+
+def main():
+    print('hello')
+    
+
+if __name__ == '__main__':
+    # 调用函数
+    main()
